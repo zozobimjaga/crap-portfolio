b33cdf622ee9e4ef8432ee07b5eebe3513b678f0
chinazom <chinazomomenife@ymail.com>
2023-06-21 03:55:12 +0100

    i_am_rich/

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
